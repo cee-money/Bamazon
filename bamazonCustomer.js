3b13c6fa0adaab9f2c4c80595f168676fe61c6ae
cmoney <moneymaker.carolyn@gmail.com>
2019-07-01 16:54:18 -0600

    logic and loggig

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,9 @@
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-//     This means updating the SQL database to reflect the remaining quantity.
-//     Once the update goes through, show the customer the total cost of their purchase.
-
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+var divider = `\n----------------------------------------------------------------------\n`
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,70 +11,104 @@ var connection = mysql.createConnection({
     user: "root",
     password: "root",
     database: "bamazon"
-  });
+});
   
-  connection.connect(function(err) {
+connection.connect(function(err) {
       if (err) throw err;
       // console.log(`\nconnected as id ${connection.threadId}\n`);
       displayAllProd();
 
-    }); 
+}); 
 
 function displayAllProd() {
     connection.query("SELECT * FROM products", function(err, results) {
         if (err) throw err;
-        console.log(`\nProducts for purchase:\n----------------------------------------------------------------------`)
-        for (var i = 0; i < results.length; i++) {
-        console.log(`ID: ${results[i].id} | Item: ${results[i].product_name} | Dept: ${results[i].department_name} | Price: $${results[i].price}`);
-        }
-        console.log(`----------------------------------------------------------------------\n`);
+        console.log(`\nProducts for purchase:\n`)
 
-    inquirer.prompt([
-        {
-            name: "id",
-            type: "list",
-            message: "Select the product ID that you would like to purchase:",
-            choices: [`ID: ${results[0].id} | Item: ${results[0].product_name}`, `ID: ${results[1].id} | Item: ${results[1].product_name}`, `ID: ${results[2].id} | Item: ${results[2].product_name}`, `ID: ${results[3].id} | Item: ${results[3].product_name}`, `ID: ${results[4].id} | Item: ${results[4].product_name}`, `ID: ${results[5].id} | Item: ${results[5].product_name}`, `ID: ${results[6].id} | Item: ${results[6].product_name}`, `ID: ${results[7].id} | Item: ${results[7].product_name}`, `ID: ${results[8].id} | Item: ${results[8].product_name}`, `ID: ${results[9].id} | Item: ${results[9].product_name}`]
-
-        },
-        {
-            name: "quantity",
-            type: "input",
-            message: "How many units of the product would you like to purchase?",
-            //validate if a number
-        }
-        ]).then(function(answers) {
-            console.log(answers);
-            // placeOrder(answers.id, answers.quantity);
+        var table = new Table({
+            head: [`ID #`, `Item Name`, `Department`, `Price`]
+          , colWidths: [7, 25, 25, 10]
         });
+
+        for (var i = 0; i < results.length; i++) {
+
+            table.push(
+                [results[i].id, results[i].product_name, results[i].department_name, `$${results[i].price}`]
+            );
+        }
+        console.log(table.toString());
+        console.log(divider);
+
+        inquirer.prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "What is the ID # of the product you would like to purchase:",
+                validate: function(value) {
+                    if (isNaN(value)) {
+                        console.log(`Please input a number`);
+                        return false;
+                    }
+                    return true
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many units of that product would you like?",
+                validate: function(value) {
+                    if (isNaN(value)) {
+                        console.log(`Please input a number`);
+                        return false;
+                    }
+                    return true
+                }
+            }
+            ]).then(function(answers) {
+                // console.log(answers);
+                placeOrder(answers);
+            });
     });
+};
+
+function placeOrder(answers) {
+
+    console.log(`${divider}\u231B  Processing your order... \n`);
+    var query = "SELECT id, product_name, price, stock_quantity FROM products WHERE ? ";
+    
+    connection.query(query, {id: answers.id}, function(err, res) {
+        if (err) throw err;
+
+        var res = res[0];
+
+        // console.log(`Item: ${answers.id}`);
+        // console.log(res.id);
+        // console.log(res.stock_quantity);
+
+        if (answers.quantity > res.stock_quantity) {
+            console.log(`Insufficient stock. Your order has been canceled.`);
+            return;
+        } else {
+            updateStock(answers);
+            console.log(`Your order for ${answers.quantity} unit(s) of ${res.product_name} has been placed.\nYour total was $` + (res.price * answers.quantity));  
+        }
+    })
+};
+
+function updateStock(answers) {
+    
+    connection.query("UPDATE products SET ? WHERE ?",
+        [ { stock_quantity: stock_quantity - answers.quantity }, { id: answers.id } ],
+        function(error) {
+          if (error) throw err;
+      
+          console.log(res.stock_quantity)            
+        }
+    );
+};
 
 
-}
-  
-
-
-    // function placeOrder(id, qty) {
-
-    //     console.log("Processing your order \n");
-    //     var query = connection.query(
-    //         "INSERT INTO greatbay SET ?",
-    //         {
-    //             category: category,
-    //             name: name,
-    //             bid: bid
-    //         },
-    //         function(err, res){
-    //             if(err) throw err;
-    //             console.log(`\n${res.affectedRows} Your order has been placed!\n`);
-    //         }
-    //     );
-    //     console.log(query.sql);
-    // };
-    //read the stock quantity for the product they selected
-    //if user input <= stock quantity, then place order and deduct stock accordingly, log price to customer which will be unit cost times the user input quantity
-    //if user input > stock quantity, then log Insufficient quantity and cancel order
-
+    
 
 
 // ----------------------------------------------------
@@ -96,4 +126,4 @@ function displayAllProd() {
 // If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
 // If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
 // If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
-// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
+// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store //
